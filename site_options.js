faa7dd3a97c1d5b8503ad92bd4149a66c33c7227
Tobias Speicher <rootcommander@gmail.com>
2018-02-13 20:10:03 +0100

    Update

--- site_options.js
+++ site_options.js
@@ -1216,15 +1216,15 @@
 		},
 		"2d6bf24d-39b8-491b-936e-4daebd3cc17c": {
 			"name": "twilight_clips_already",
-			"v": 2999,
+			"v": 3049,
 			"t": 1,
 			"groups": [
 				{
-					"weight": 100,
+					"weight": 0,
 					"value": "off"
 				},
 				{
-					"weight": 0,
+					"weight": 100,
 					"value": "on"
 				}
 			]
