cf317355b52f057ce6d51b777e00bb3b1dd2e063
Tobias Speicher <rootcommander@gmail.com>
2020-10-12 21:12:26 +0200

    Remove site_options.js as the file is not changed by Twitch anymore Handle curl erros where the experiments.json is empty

--- run.sh
+++ run.sh
@@ -2,11 +2,13 @@
 
 # Get experiments.json
 curl --connect-timeout 10 -s --compressed 'https://www.twitch.tv/experiments.json' | jq -M --tab . > experiments.json
-git add experiments.json && git commit -m 'Update experiments.json'
+if [ -s experiments.json ]; then
+	git add experiments.json && git commit -m 'Update experiments.json'
+fi
 
 # Get site_options.js
-curl --connect-timeout 10 -s --compressed 'https://www.twitch.tv/site_options.js' | tail -c +22 | head -c -2 | jq -M --tab . > site_options.js
-git add site_options.js && git commit -m 'Update site_options.js'
+# curl --connect-timeout 10 -s --compressed 'https://www.twitch.tv/site_options.js' | tail -c +22 | head -c -2 | jq -M --tab . > site_options.js
+# git add site_options.js && git commit -m 'Update site_options.js'
 
 # Push to repo
 git push
