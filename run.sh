7fad3b7c5bd6e058d8d132ecd4d7cc811db63fcc
CommanderRoot <CommanderRoot@users.noreply.github.com>
2026-01-31 21:39:20 +0100

    Add script to fetch and commit config.json
    Fetch config from Twitch API and commit if present.

--- run.sh
+++ run.sh
@@ -10,5 +10,11 @@ fi
 # curl --connect-timeout 10 -s --compressed 'https://www.twitch.tv/site_options.js' | tail -c +22 | head -c -2 | jq -M --tab . > site_options.js
 # git add site_options.js && git commit -m 'Update site_options.js'
 
+# Get config
+curl --connect-timeout 10 -s --compressed 'https://assets.twitch.tv/eppo/api/flag-config/v1/config?sdkName=js-sdk-client&sdkVersion=3.1.2&apiKey=3mv-BKCSZJDyZrwfqK86har6u13NgxRh.ZWg9bTRsZHJnLmUuZXBwby5jbG91ZCZjcz1tNGxkcmc' | jq -M --tab . > config.json
+if [ -s config.json ]; then
+	git add config.json && git commit -m 'Update config.json'
+fi
+
 # Push to repo
 git push
