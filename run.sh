924fb70d33c779bb599520adc0349a502d648ae4
CommanderRoot <CommanderRoot@users.noreply.github.com>
2026-02-03 20:41:11 +0100

    Disable fetching of experiments
    experiments.json has been removed by Twitch

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Get experiments.json
-curl --connect-timeout 10 -s --compressed 'https://www.twitch.tv/experiments.json' | jq -M --tab . > experiments.json
-if [ -s experiments.json ]; then
-	git add experiments.json && git commit -m 'Update experiments.json'
-fi
+# curl --connect-timeout 10 -s --compressed 'https://www.twitch.tv/experiments.json' | jq -M --tab . > experiments.json
+# if [ -s experiments.json ]; then
+#	git add experiments.json && git commit -m 'Update experiments.json'
+# fi
 
 # Get site_options.js
 # curl --connect-timeout 10 -s --compressed 'https://www.twitch.tv/site_options.js' | tail -c +22 | head -c -2 | jq -M --tab . > site_options.js
