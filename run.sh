f7d1431abd0951ddf2692520eb7b57b2c7d1a2b5
CommanderRoot <CommanderRoot@users.noreply.github.com>
2026-06-08 01:06:22 +0200

    Fetch global badges and update global_badges.json
    Add functionality to fetch and save global badges.

--- run.sh
+++ run.sh
@@ -16,5 +16,11 @@ if [ -s config.json ]; then
 	git add config.json && git commit -m 'Update config.json'
 fi
 
+# Get global badges
+curl --connect-timeout 10 -s --compressed --request POST --header 'Client-Id: kimne78kx3ncx6brgo4mv6wki5h1ko' --header 'Content-Type: application/json' --data '{"query":"query GetGlobalBadges { badges { id setID version title description imageURL onClickAction clickURL }}","operationName":"GetGlobalBadges","variables":{}}' 'https://gql.twitch.tv/gql' | jq -M --tab > global_badges.json
+if [ -s global_badges.json ]; then
+	git add global_badges.json && git commit -m 'Update global_badges.json'
+fi
+
 # Push to repo
 git push
